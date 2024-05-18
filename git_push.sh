3d09af603e8a9b444c371481966ad47220adc66b
fgwsz <3191000458@qq.com>
2024-05-18 20:49:10 +0800

    add_ubuntu_hotkey

---
+++ git_push.sh
@@ -0,0 +1,9 @@
+echo "you can input exit to abort git push."
+read -p "input commit info: " commit_info
+if [ "$commit_info" = "exit" ]; then
+    echo "git push exit!"
+else
+   git add .
+   git commit -m "$commit_info"
+   git push
+fi
