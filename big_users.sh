46cc87048139f5077ad5ad53411b7db4f881e054
fgwsz <3191000458@qq.com>
2026-07-14 23:26:53 +0800

    增加big_users.sh深度和容量信息

--- big_users.sh
+++ big_users.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
-
 # big users - Find big disk space users in various directories
 ##############################################################################
 # Parameters for script
-CHECK_DIRECTORIES=" /var/log /home" #Directories to check
+CHECK_DIRECTORIES=" /var/log /home"      # Directories to check
 ############################## main script ###################################
-DATE=$(date '+%m%d%y')                  #Date for report file
-exec > disk_space_$DATE.rpt             #Make report file STDOUT
-echo "Top Ten Disk Sapce Usage"         #Report header
+DATE=$(date '+%m%d%y')                   # Date for report file
+exec > disk_space_$DATE.rpt              # Make report file STDOUT
+echo "Top Ten Disk Space Usage"          # Report header
 echo "for $CHECK_DIRECTORIES Directories"
-for DIR_CHECK in $CHECK_DIRECTORIES     #Loop to do directories
+for DIR_CHECK in $CHECK_DIRECTORIES      # Loop to do directories
 do
     echo ""
-    echo "The $DIR_CHECK Directory:"    #Directory header
+    echo "The $DIR_CHECK Directory:"     # Directory header
     # Create a listing of top ten disk space users in this dir
-    du -S $DIR_CHECK 2>/dev/null |
-    sort -rn |
-    sed '{11,$D; =}' |
-    sed 'N; s/\n/ /' |
-    gawk '{printf $1 ":" "\t" $2 "\t" $3 "\n"}'
-done                                    #End of loop
+    du -b -S "$DIR_CHECK" 2>/dev/null |  # -b: bytes, -S: separate dirs
+    sort -nr |                           # numeric sort descending
+    head -100 |                          # keep only top 100
+    awk -F'\t' '                         # fields separated by tab
+    {
+        size = $1
+        if (size >= 1024*1024*1024) {
+            hum = sprintf("%.1fG", size/1024/1024/1024)
+        } else if (size >= 1024*1024) {
+            hum = sprintf("%.1fM", size/1024/1024)
+        } else if (size >= 1024) {
+            hum = sprintf("%.1fK", size/1024)
+        } else {
+            hum = sprintf("%dB", size)
+        }
+        printf "%d:\t%s\t%s\n", NR, hum, $2
+    }'
+done
 exit
